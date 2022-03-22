2dc93bffb91b6c65678af629aeb9f924db6b17ea
cesarzxk <43748428+cesarzxk@users.noreply.github.com>
2022-03-22 18:34:51 -0300

    Implementado a página Checkout
    Implementada página que será exibida de acordo com o status da requisição gerado pelo pedido.

---
+++ src/Pages/Checkout/index.tsx
@@ -0,0 +1,63 @@
+
+import { 
+    Wrap, 
+    Alert,
+    AlertIcon,
+    AlertTitle,
+    AlertDescription
+  
+  } from '@chakra-ui/react';
+  
+ 
+  function Checkout() {
+      const [status, setStatus] = useState(0)
+
+      useEffect(()=>{
+        executeOrder()
+      }, [])
+
+      async function executeOrder() {
+        const curretStatus = await setOrder()
+        if(curretStatus == 200){
+            setItemsCart([])
+
+        }
+
+        setStatus(curretStatus)
+      }
+
+    
+  
+    return (
+      <Wrap
+      justify='center'
+      w='70%'
+      h='95%'
+      overflowY='hidden'
+      overflowX='hidden'>
+
+          <Alert
+            status='success'
+            variant='subtle'
+            flexDirection='column'
+            alignItems='center'
+            justifyContent='center'
+            textAlign='center'
+            h='85vh'
+            >
+                <AlertIcon boxSize='40px' mr={0} />
+                <AlertTitle mt={4} mb={1} fontSize='lg'>
+                    Compra completada com sucesso!
+                </AlertTitle>
+                <AlertDescription maxWidth='sm'>
+                   O pedido foi realizado com sucesso! 
+                   Obrigado e volte sempre!
+                </AlertDescription>
+            </Alert>
+       
+      </Wrap>
+    );
+  }
+  
+  export default Checkout;
+  
