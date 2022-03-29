7862a7ebd00f55947ae031ae260b2fe6b84e7263
cesarzxk <43748428+cesarzxk@users.noreply.github.com>
2022-03-29 16:44:26 -0300

    Update index.ts

--- src/Api/index.ts
+++ src/Api/index.ts
@@ -1,29 +1,35 @@
-import axios from 'axios';
+import axios from "axios";
 
-export const order  = axios.create({
-    baseURL:'http://localhost:3333/order',
-    headers:{
-        'Content-Type':'application/json;charset=utf-8'
-    }
-})
+const development = false;
 
-export const authenticate  = axios.create({
-    baseURL:'http://localhost:3333/authenticate',
-    headers:{
-        'Content-Type':'application/json;charset=utf-8'
-    }
-})
+const baseurl = development
+  ? "http://localhost:3333/"
+  : "https://fifty-cents-store.herokuapp.com/";
 
-export const register  = axios.create({
-    baseURL:'http://localhost:3333/register',
-    headers:{
-        'Content-Type':'application/json;charset=utf-8'
-    }
-})
+export const order = axios.create({
+  baseURL: baseurl + "order",
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
+});
 
-export const items  = axios.create({
-    baseURL:'http://localhost:3333/items',
-    headers:{
-        'Content-Type':'application/json;charset=utf-8'
-    }
-})
+export const authenticate = axios.create({
+  baseURL: baseurl + "authenticate",
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
+});
+
+export const register = axios.create({
+  baseURL: baseurl + "register",
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
+});
+
+export const items = axios.create({
+  baseURL: baseurl + "items",
+  headers: {
+    "Content-Type": "application/json;charset=utf-8",
+  },
+});
