0e816de313492674dfec174efad50a2dab7a63bf
cesarzxk <cesar3d.vargas@gmail.com>
2024-08-22 02:17:05 -0300

    refactor(change): :art: header rebranding

--- src/Containers/Header/index.tsx
+++ src/Containers/Header/index.tsx
@@ -1,56 +1,76 @@
-import { SearchIcon } from "@chakra-ui/icons";
-import { Img, Spacer, HStack, IconButton } from "@chakra-ui/react";
+import { HamburgerIcon, SearchIcon } from "@chakra-ui/icons";
+import {
+  Spacer,
+  HStack,
+  IconButton,
+  CloseButton,
+  Flex,
+  Image,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import Search from "../../Components/Search";
+import { useGlobal } from "../../Context/Global/GlobalContext";
 import useDimensions from "../../Hooks/useDimensions";
 import Cart from "../Cart";
 import User from "../User";
+import Logo from "./Logo.svg";
 
 function Header() {
   const navigate = useNavigate();
   const dimensions = useDimensions();
+  const { onFiltersOpen } = useGlobal();
   const [isSearch, setIsSearch] = useState(false);
-
-  function switchWidth() {
-    if (dimensions.width >= 1000) {
-      return "70%";
-    } else {
-      return "100%";
-    }
-  }
 
   return (
     <HStack
-      bg="gray.300"
-      w={switchWidth()}
-      h="5rem"
-      marginTop={dimensions.width >= 1000 ? "1rem" : undefined}
-      marginBottom="1rem"
-      paddingX="1rem"
+      bg="black"
+      w="100%"
+      h="3rem"
+      paddingX={{ base: "1rem", sm: "1.5rem", md: "2rem" }}
       justifyContent="center"
       alignItems="center"
     >
-      <Img h="5rem" src="./logo.svg" onClick={() => navigate("/")} />
-
-      <Spacer />
+      {dimensions.width <= 1000 && (
+        <HamburgerIcon
+          color="white"
+          height="2.5rem"
+          width="2.5rem"
+          cursor="pointer"
+          onClick={onFiltersOpen}
+        />
+      )}
 
       {isSearch ? (
-        <Search autoFocus={true} onBlur={() => setIsSearch(false)} />
+        <HStack alignItems="center" w="100%">
+          <Search autoFocus={true} onBlur={() => setIsSearch(false)} />
+          <CloseButton
+            data-testid="exitSearch"
+            aria-label="Exit Search Mode"
+            borderRadius="100%"
+            color="white"
+            border="1px solid red"
+            onClick={() => setIsSearch(false)}
+            height="2.5rem"
+            width="2.5rem"
+          />
+        </HStack>
       ) : (
         <>
-          {dimensions.width >= 400 ? (
+          <Flex onClick={() => navigate("/")} cursor="pointer">
+            <Image src={Logo} h="3rem" />
+          </Flex>
+          <Spacer />
+          {dimensions.width >= 500 ? (
             <Search />
           ) : (
             <IconButton
               colorScheme="yellow"
-              aria-label="Search database"
-              h="3rem"
-              w="3rem"
               data-testid="setSeachButton"
-              borderRadius="100%"
+              aria-label="Search database"
               icon={<SearchIcon />}
+              borderRadius="100%"
               onClick={() => setIsSearch(true)}
             />
           )}
